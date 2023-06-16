54c0b589f31d156015068ab9e2654816d6cda855
Oğuzhan Dikici <oguzhandikici97@gmail.com>
2023-06-16 19:35:52 +0300

    gelistirmeler

--- models/skts_registration.py
+++ models/skts_registration.py
@@ -1,10 +1,17 @@
 from odoo import models, fields, api, _
+from odoo.exceptions import UserError
 
 
 class SKTSPersonRegistration(models.Model):
     _name = "skts.registration"
     _description = "Registrations"
 
+    state = fields.Selection([
+        ("application", "Application"),
+        ("approved_rejected", "Approved/Rejected"),
+        ("approved", "Approved"),
+        ("rejected", "Rejected")
+    ], default="application")
     person_id = fields.Many2one("skts.person", required=True)
 
     person_name = fields.Char(related="person_id.name", readonly=False)
@@ -17,8 +24,30 @@ class SKTSPersonRegistration(models.Model):
     person_contact_ids = fields.One2many(related="person_id.contact_ids", readonly=False)
 
     school_id = fields.Many2one("skts.school", required=True, ondelete="restrict")
+    type_id = fields.Many2one("skts.school.registration.type", string="Registration Type", required=True)
     school_term_ids = fields.Many2many("skts.school.term", "skts_registration_school_term_rel", required=True,
                                        string="School Terms", domain="[('school_id', '=', school_id), ('open_to_register', '=', True)]")
-    type_id = fields.Many2one("skts.school.registration.type", string="Registration Type")
+    school_terms_display = fields.Char(compute="_compute_school_terms_display")
+    @api.depends("school_term_ids")
+    def _compute_school_terms_display(self):
+        for record in self:
+            computed_name = ""
+            for term_id in record.school_term_ids:
+                if computed_name and term_id:
+                    computed_name += ' + '
+                if term_id:
+                    computed_name += term_id.name
+            record.school_terms_display = computed_name
 
     note = fields.Text(string="Registration Note")
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        if vals_list:
+            for value in vals_list:
+                if not value["school_term_ids"][0][2]:
+                    raise UserError(_("You must add at least 1 school term"))
+        return super().create(vals_list)
+
+    def approve(self):
+        self.state = "approved"
